fdb8888b140b946af4c5d53daec849164f338bfa
hasunghwa <tjdghk3920@naver.com>
2022-05-12 16:43:41 +0900

    directory location change

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import typeDefs from "./typeDefs.js";
 import resolvers from "./resolvers.js";
-
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
 async function initServer() {
+  const PORT = process.env.PORT || 5000;
   const app = express();
   app.use(cors());
   dotenv.config();
@@ -16,13 +16,13 @@ async function initServer() {
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
 
-  const PORT = process.env.PORT || 5000;
-   try {
-     await mongoose.connect(process.env.MONGODB_URL);
-     console.log(`Connected to MongoDB`);
-   } catch (error) {
-     console.log(error);
-   }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`Connected to MongoDB`);
+  } catch (error) {
+    console.log(error);
+  }
+
   app.listen(PORT, () =>
     console.log(`Express server is running on port ${PORT}`)
   );
